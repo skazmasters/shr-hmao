2628a9a4179050bffd701ec881701a8b7a72e629
Eugene <eugene.kotsarev@gmail.com>
2021-01-29 22:36:47 +0300

    remove deps

--- app/js/vendors.js
+++ app/js/vendors.js
@@ -1,18 +1,18 @@
-import 'react-app-polyfill/stable';
-import 'react-app-polyfill/ie11';
-import 'intersection-observer';
+// import 'react-app-polyfill/stable';
+// import 'react-app-polyfill/ie11';
+// import 'intersection-observer';
 import './polyfills';
-import 'html5shiv';
-import 'picturefill';
+// import 'html5shiv';
+// import 'picturefill';
 import lazySizes from 'lazysizes';
 import 'lazysizes/plugins/native-loading/ls.native-loading';
 import 'lazysizes/plugins/object-fit/ls.object-fit';
 import svgPolyfill from 'svg4everybody';
 import jquery from 'jquery';
 import Swiper from 'swiper/bundle';
-import imask from 'imask';
-import datepicker from 'js-datepicker';
-import { format as dateFormat } from 'date-fns';
+// import imask from 'imask';
+// import datepicker from 'js-datepicker';
+// import { format as dateFormat } from 'date-fns';
 import select2 from 'select2';
 import PerfectScrollbar from 'perfect-scrollbar';
 import StickySidebar from './src/modules/sidebar';
@@ -22,15 +22,15 @@ import PhotoSwipeUI from 'photoswipe/dist/photoswipe-ui-default';
 window.$ = window.jQuery = jquery;
 window.svg4everybody = svgPolyfill;
 window.Swiper = Swiper;
-window.IMask = imask;
+// window.IMask = imask;
 window.select2 = select2;
 window.PerfectScrollbar = PerfectScrollbar;
 
 window.StickySidebar = StickySidebar;
-window.datepicker = datepicker;
-window.dateHelper = {
-  format: dateFormat,
-};
+// window.datepicker = datepicker;
+// window.dateHelper = {
+//   format: dateFormat,
+// };
 
 window.PhotoSwipe = PhotoSwipe;
 window.PhotoSwipeUI = PhotoSwipeUI;
